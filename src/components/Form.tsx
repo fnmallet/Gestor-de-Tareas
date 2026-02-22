7cfef55ed0d89a0dd1a5fcd4965fc0b14be32bbb
Fernando Mallet <fernandomallet@outlook.com>
2026-02-22 16:28:06 -0300

    chore: add Form component

---
+++ src/components/Form.tsx
@@ -0,0 +1,26 @@
+import { cn } from '@/utils/cn';
+import {  ReactNode } from 'react';
+import { FieldValues, SubmitHandler, UseFormReturn } from 'react-hook-form';
+
+type FormProps<T extends FieldValues> = {
+  action: (formData: FormData) => Promise<void>;
+  children: ReactNode;
+  onSubmit: SubmitHandler<T>;
+  methods: UseFormReturn<T>;
+  className?: string;
+}
+
+const Form = <T extends FieldValues>({ action, children, onSubmit, methods, className }: FormProps<T>) => {
+  return <form
+    action={action}
+    onSubmit={e => {
+      e.preventDefault();
+      void methods.handleSubmit(onSubmit)(e);
+    }}
+    className={cn('flex flex-col gap-4', className)}
+  >
+    {children}
+  </form>;
+};
+
+export default Form;
