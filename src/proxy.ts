0c669d2ed9677efc682e4b6e59490267bee90e14
Fernando Mallet <fernandomallet@outlook.com>
2026-02-22 17:29:04 -0300

    chore: proxy

---
+++ src/proxy.ts
@@ -0,0 +1,8 @@
+import NextAuth from 'next-auth';
+import { authConfig } from '@/features/auth/auth';
+
+export default NextAuth(authConfig).auth;
+ 
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.(?:png|jpg|jpeg|gif|svg|webp)$).*)'],
+};
