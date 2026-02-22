d94fd6991684a1db9437cc99a44bdb37090cca2e
Fernando Mallet <fernandomallet@outlook.com>
2026-02-22 19:58:41 -0300

    feat: login and register pages

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+import LoginForm from '@/features/auth/login/components/LoginForm';
+
+const LoginPage = () => {
+  return <LoginForm />;
+};
+
+export default LoginPage;
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,7 @@
+import RegisterForm from '@/features/auth/register/components/RegisterForm';
+
+const RegisterPage = () => {
+  return <RegisterForm />;
+};
+
+export default RegisterPage;
