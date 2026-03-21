dd83f3a41bc614997f7f270965649c208b1d6362
Fernando Mallet <fernandomallet@outlook.com>
2026-03-20 22:01:16 -0300

    chore: add red button style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,3 @@
-import { cn } from '@/utils/cn';
-
 type Button = {
   children: React.ReactNode;
   type?: 'button' | 'submit' | 'reset';
@@ -8,16 +6,18 @@ type Button = {
   disabled?: boolean;
 }
 
-export const buttonClasses = 'transition-colors duration-300 bg-blue-500 hover:bg-white hover:outline hover:outline-blue-500 hover:text-blue-500 cursor-pointer py-1 px-4 rounded-md text-white hover:shadow-md hover:shadow-blue-200 text-center';
+export const buttonClasses = 'transition-colors duration-300 bg-blue-500 hover:bg-white hover:outline hover:outline-blue-500 hover:text-blue-500 cursor-pointer py-2 px-4 rounded-xl text-white hover:shadow-md hover:shadow-blue-200 text-center';
+
+export const buttonClassesRedOutline = 'transition-colors duration-300 bg-red-500 hover:bg-white hover:outline hover:outline-red-500 hover:text-red-500 cursor-pointer py-2 px-4 rounded-xl text-white hover:shadow-md hover:shadow-red-200 text-center'; 
 
 const Button = ({ children, type, className, isLoading, disabled }: Button) => {
   return <button 
     type={type} 
     disabled={disabled}
-    className={cn('transition-colors duration-300 bg-blue-500 hover:bg-white hover:outline hover:outline-blue-500 hover:text-blue-500 cursor-pointer py-1 px-4 rounded-md text-white hover:shadow-md hover:shadow-blue-200', className)}
+    className={className}
   >
     {isLoading ? <span
-      className="inline-block h-full aspect-square animate-spin rounded-full border-2 border-white border-t-transparent"
+      className="inline-block h-full aspect-square animate-spin rounded-full border-2  hover:border-blue-500 border-t-transparent"
     ></span> : children}
   </button>;
 };
